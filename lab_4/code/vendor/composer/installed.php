ee158a8257b28fdf6c64fe288c55db2834733e0b
Artem Dyrdin <74466573+ArtemDyrdin@users.noreply.github.com>
2024-05-03 22:20:21 +0300

    lab4: installed composer

---
+++ lab_4/code/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'web/lab_4',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '03a1a97466cb5bae7e530ecd0e3e1e0b6deac378',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'web/lab_4' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '03a1a97466cb5bae7e530ecd0e3e1e0b6deac378',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
